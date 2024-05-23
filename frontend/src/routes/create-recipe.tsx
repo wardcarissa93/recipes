bb17b70a3678024d348be24e001c3bf44be6872f
Carissa Ward <cward52@my.bcit.ca>
2024-05-22 17:51:54 -0700

    updated create recipe form

--- frontend/src/routes/create-recipe.tsx
+++ frontend/src/routes/create-recipe.tsx
@@ -1,13 +1,207 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, useNavigate } from '@tanstack/react-router'
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Button } from '@/components/ui/button'
+import { useForm } from '@tanstack/react-form'
+import { api } from '@/lib/api'
+import { zodValidator } from '@tanstack/zod-form-adapter'
 
 export const Route = createFileRoute('/create-recipe')({
     component: CreateRecipe
 })
 
 function CreateRecipe() {
+    const navigate = useNavigate();
+    const form = useForm({
+        validatorAdapter: zodValidator,
+        defaultValues: {
+            title: '',
+            description: '',
+            prepTime: 0,
+            cookTime: 0,
+            totalTime: 0,
+            servings: 0,
+            instructions: '',
+            url: ''
+        },
+        onSubmit: async ({ value }) => {
+            const res = await api.recipes.$post({ json: value });
+            if (!res.ok) {
+                throw new Error("server error")
+            }
+            navigate({to: "/recipes"})
+        }
+    })
     return (
         <div className="p-2">
-            <h3>Create recipe</h3>
+            <h2>Create Recipe</h2>
+            <form 
+                className='max-w-xl m-auto'
+                onSubmit={(e) => {
+                    e.preventDefault()
+                    e.stopPropagation()
+                    void form.handleSubmit()
+                }}
+            >
+                <form.Field 
+                    name="title"
+                    children={((field) => (
+                        <>
+                            <Label htmlFor={field.name}>Title</Label>
+                            <Input
+                                id={field.name}
+                                name={field.name}
+                                value={field.state.value}
+                                onBlur={field.handleBlur}
+                                onChange={(e) => field.handleChange(e.target.value)}
+                            />
+                            {field.state.meta.touchedErrors ? (
+                                <em>{field.state.meta.touchedErrors}</em>
+                            ) : null}
+                        </>
+                    ))}
+                />
+                <form.Field 
+                    name="description"
+                    children={((field) => (
+                        <>
+                            <Label htmlFor={field.name}>Description</Label>
+                            <Input
+                                id={field.name}
+                                name={field.name}
+                                value={field.state.value}
+                                onBlur={field.handleBlur}
+                                onChange={(e) => field.handleChange(e.target.value)}
+                            />
+                            {field.state.meta.touchedErrors ? (
+                                <em>{field.state.meta.touchedErrors}</em>
+                            ) : null}
+                        </>
+                    ))}
+                />
+                <form.Field 
+                    name="prepTime"
+                    children={((field) => (
+                        <>
+                            <Label htmlFor={field.name}>Prep Time</Label>
+                            <Input
+                                id={field.name}
+                                name={field.name}
+                                value={field.state.value}
+                                onBlur={field.handleBlur}
+                                type="number"
+                                onChange={(e) => field.handleChange(Number(e.target.value))}
+                            />
+                            {field.state.meta.touchedErrors ? (
+                                <em>{field.state.meta.touchedErrors}</em>
+                            ) : null}
+                        </>
+                    ))}
+                />
+                <form.Field 
+                    name="cookTime"
+                    children={((field) => (
+                        <>
+                            <Label htmlFor={field.name}>Cook Time</Label>
+                            <Input
+                                id={field.name}
+                                name={field.name}
+                                value={field.state.value}
+                                onBlur={field.handleBlur}
+                                type="number"
+                                onChange={(e) => field.handleChange(Number(e.target.value))}
+                            />
+                            {field.state.meta.touchedErrors ? (
+                                <em>{field.state.meta.touchedErrors}</em>
+                            ) : null}
+                        </>
+                    ))}
+                />
+                <form.Field 
+                    name="totalTime"
+                    children={((field) => (
+                        <>
+                            <Label htmlFor={field.name}>Total Time</Label>
+                            <Input
+                                id={field.name}
+                                name={field.name}
+                                value={field.state.value}
+                                onBlur={field.handleBlur}
+                                type="number"
+                                onChange={(e) => field.handleChange(Number(e.target.value))}
+                            />
+                            {field.state.meta.touchedErrors ? (
+                                <em>{field.state.meta.touchedErrors}</em>
+                            ) : null}
+                        </>
+                    ))}
+                />
+                <form.Field 
+                    name="servings"
+                    children={((field) => (
+                        <>
+                            <Label htmlFor={field.name}>Servings</Label>
+                            <Input
+                                id={field.name}
+                                name={field.name}
+                                value={field.state.value}
+                                onBlur={field.handleBlur}
+                                type="number"
+                                onChange={(e) => field.handleChange(Number(e.target.value))}
+                            />
+                            {field.state.meta.touchedErrors ? (
+                                <em>{field.state.meta.touchedErrors}</em>
+                            ) : null}
+                        </>
+                    ))}
+                />
+                <form.Field 
+                    name="instructions"
+                    children={((field) => (
+                        <>
+                            <Label htmlFor={field.name}>Instructions</Label>
+                            <Input
+                                id={field.name}
+                                name={field.name}
+                                value={field.state.value}
+                                onBlur={field.handleBlur}
+                                onChange={(e) => field.handleChange(e.target.value)}
+                            />
+                            {field.state.meta.touchedErrors ? (
+                                <em>{field.state.meta.touchedErrors}</em>
+                            ) : null}
+                        </>
+                    ))}
+                />
+                <form.Field 
+                    name="url"
+                    children={((field) => (
+                        <>
+                            <Label htmlFor={field.name}>URL</Label>
+                            <Input
+                                id={field.name}
+                                name={field.name}
+                                value={field.state.value}
+                                onBlur={field.handleBlur}
+                                onChange={(e) => field.handleChange(e.target.value)}
+                            />
+                            {field.state.meta.touchedErrors ? (
+                                <em>{field.state.meta.touchedErrors}</em>
+                            ) : null}
+                        </>
+                    ))}
+                />
+                <form.Subscribe
+                    selector={(state) => [state.canSubmit, state.isSubmitting]}
+                    children={([canSubmit, isSubmitting]) => (
+                        <Button className="mt-4" type="submit" disabled={!canSubmit}>
+                            {isSubmitting ? "..." : "Submit"}
+                        </Button>
+                    )}
+                >
+                    
+                </form.Subscribe>
+            </form>
         </div>
     )
 }
