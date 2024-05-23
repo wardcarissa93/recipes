1b3dd063827a36e208039f3a2de774a2f5ee5597
Carissa Ward <cward52@my.bcit.ca>
2024-05-22 18:18:48 -0700

    finished kinde auth for recipes

--- server/app.ts
+++ server/app.ts
@@ -2,6 +2,7 @@ import { Hono } from 'hono'
 import { logger } from 'hono/logger'
 import { serveStatic } from 'hono/bun'
 import { recipesRoute } from './routes/recipes'
+import { authRoute } from './routes/auth'
 
 const app = new Hono()
 
@@ -11,7 +12,7 @@ app.get("/test", c => {
     return c.json({"message": "test"})
 })
 
-const apiRoutes = app.basePath("/api").route("/recipes", recipesRoute)
+const apiRoutes = app.basePath("/api").route("/recipes", recipesRoute).route("/", authRoute)
 
 app.get('*', serveStatic({ root: './frontend/dist' }))
 app.get('*', serveStatic({ path: './frontend/dist/index.html' }))
