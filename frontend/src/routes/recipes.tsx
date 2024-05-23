d18d1101580dcb67da522e79ccd73918c25e84bb
Carissa Ward <cward52@my.bcit.ca>
2024-05-22 17:41:00 -0700

    updated recipes.tsx to show all recipes

--- frontend/src/routes/recipes.tsx
+++ frontend/src/routes/recipes.tsx
@@ -1,9 +1,7 @@
 import { 
     Table,
     TableBody,
-    TableCaption,
     TableCell,
-    TableFooter,
     TableHead,
     TableHeader,
     TableRow
@@ -17,10 +15,50 @@ export const Route = createFileRoute('/recipes')({
     component: Recipes
 })
 
+async function getAllRecipes() {
+    const res = await api.recipes.$get()
+    if (!res.ok) {
+        throw new Error('server error')
+    }
+    const data = await res.json()
+    return data
+}
+
 function Recipes() {
+    const { isPending, error, data } = useQuery({
+        queryKey: ['get-all-recipes'],
+        queryFn: getAllRecipes
+    })
+
+    if (error) return 'An error has occurred: ' + error.message
     return (
-        <div className="p-2">
-            <h3>Show all recipes</h3>
+        <div className="p-2 max-w-3xl m-auto">
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="w-[100px]">Id</TableHead>
+                        <TableHead>Title</TableHead>
+                        <TableHead>Servings</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {isPending 
+                    ? Array(3).fill(0).map((_, i) => (
+                        <TableRow key={i}>
+                            <TableCell className="font-medium"><Skeleton className="h-4"></Skeleton></TableCell>
+                            <TableCell><Skeleton className="h-4"></Skeleton></TableCell>
+                            <TableCell><Skeleton className="h-4"></Skeleton></TableCell>
+                        </TableRow>
+                    )) 
+                    : data?.recipes.map((recipe) => (
+                        <TableRow key={recipe.id}>
+                            <TableCell className="font-medium">{recipe.id}</TableCell>
+                            <TableCell>{recipe.title}</TableCell>
+                            <TableCell>{recipe.servings}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
         </div>
     )
 }
