b7e93f73467cd13f4e6be424f0caf52a9b4bf7b7
Carissa Ward <cward52@my.bcit.ca>
2024-05-23 18:37:15 -0700

    updated profile.tsx

--- frontend/src/routes/_authenticated/profile.tsx
+++ frontend/src/routes/_authenticated/profile.tsx
@@ -1,6 +1,8 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { useQuery } from '@tanstack/react-query'
 import { userQueryOptions } from "@/lib/api"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Button } from '@/components/ui/button'
 
 export const Route = createFileRoute('/_authenticated/profile')({
     component: Profile
@@ -14,8 +16,20 @@ function Profile() {
 
     return (
         <div className="p-2">
-            <h3>Profile Page</h3>
-            <p>Hello, {data.user.given_name}</p>
+            <div className="flex items-center gap-2">
+                <Avatar>
+                    {data.user.picture && (
+                        <AvatarImage src={data.user.picture} alt={data.user.given_name}/>
+                    )}
+                    <AvatarFallback>{data.user.given_name}</AvatarFallback>
+                </Avatar>
+                <p>
+                    {data.user.given_name} {data.user.family_name}
+                </p>
+            </div>
+            <Button className="my-4">
+                <a href="/api/logout">Logout</a>
+            </Button>
         </div>
     )
 }
